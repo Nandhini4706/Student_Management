fafa47e4c4870482aac185abfb8d7e3ef307fd18
Nandhini M <nandhinimm4706@gmail.com>
2026-02-16 00:03:21 +0530

    classes

--- src/Model/Student.java
+++ src/Model/Student.java
@@ -1,4 +1,43 @@
 package Model;
 
 public class Student {
+    private String StudId;
+    private String StudName;
+    private String StudBirth;
+    private String StudYear;
+    private String StudDept;
+    private String StudMark;
+
+    public Student(String StudId, String StudName, String StudBirth, String StudYear, String StudDept, String StudMark) {
+        StudId = StudId;
+        StudName = StudName;
+         StudBirth = StudBirth;
+          StudYear = StudYear;
+           StudMark = StudMark;
+            StudDept = StudDept;
+    }
+
+    public String getStudId() {
+        return StudId;
+    }
+
+    public String getStudName() {
+        return StudName;
+    }
+
+    public String getStudBirth() {
+        return StudBirth;
+    }
+
+    public String getStudYear() {
+        return StudYear;
+    }
+
+    public String getStudDept() {
+        return StudDept;
+    }
+
+    public String getStudMark() {
+        return StudMark;
+    }
 }
