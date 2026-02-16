ebe3a67234a4cd901e65d8d1c09c7bb4810920cd
Nandhini M <nandhinimm4706@gmail.com>
2026-02-16 22:08:54 +0530

    classes

--- src/View/TeacherDataView.java
+++ src/View/TeacherDataView.java
@@ -1,5 +1,19 @@
 package View;
-
+import Model.Teacher;
 public class TeacherDataView {
+       private String DataTechId;
+       private TeacherDataView TeacherData;
 
+    public TeacherDataView(String DataTechId,String TeacherDate) {
+        DataTechId = DataTechId;
+        TeacherDate=TeacherDate;
+    }
+
+    public String getDataTechId() {
+        return DataTechId;
+    }
+
+    public TeacherDataView getTeacherData() {
+        return TeacherData;
+    }
 }
