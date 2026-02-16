635de88507bde3294a95a8b3ad8f83915841401f
Nandhini M <nandhinimm4706@gmail.com>
2026-02-16 22:04:31 +0530

    classes

--- src/View/StudDataView.java
+++ src/View/StudDataView.java
@@ -1,4 +1,19 @@
 package View;
-
+import Model.Student;
 public class StudDataView {
+    private String DataId;
+    private Student students;
+
+    public StudDataView(String dataId,String students) {
+        DataId = dataId;
+        students=students;
+    }
+
+    public String getDataId() {
+        return DataId;
+    }
+
+    public Student getStudents() {
+        return students;
+    }
 }
