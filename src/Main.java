180bd351231001b1f19c3aa2f2c85167a95f34c8
Nandhini M <nandhinimm4706@gmail.com>
2026-02-23 23:13:00 +0530

    classes

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main{
     public static void main(String[]args){
         DAO_Stud dao=new DAO_Stud();
         Student s=new Student("1","Nandhini","20.07.2006","3rd", "IT","99");
-        dao.addStudent(s);
-        dao.viewStudent();
+//        dao.addStudent(s);
+//        dao.viewStudent();
     }
 }
