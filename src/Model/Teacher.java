1046513c4df5dd9acf339ed2503fb772d573b0eb
Nandhini M <nandhinimm4706@gmail.com>
2026-02-24 23:11:02 +0530

    classes

--- src/Model/Teacher.java
+++ src/Model/Teacher.java
@@ -5,12 +5,14 @@ public class Teacher {
     private String TeacherName;
     private String TeacherDept;
     private String Subject;
+    private String YearJoin;
 
-    public Teacher(String TeacherId,String TeacherName, String TeacherDept, String Subject) {
+    public Teacher(String TeacherId,String TeacherName, String TeacherDept, String Subject, String YearJoin) {
         TeacherId = TeacherId;
         TeacherName = TeacherName;
         TeacherDept = TeacherDept;
         Subject=Subject;
+        YearJoin=YearJoin;
     }
 
     public String getTeacherId() {
@@ -25,7 +27,12 @@ public class Teacher {
         return TeacherDept;
     }
 
+    public String getYearJoin() {
+        return YearJoin;
+    }
+
     public String getSubject() {
         return Subject;
     }
+
 }
