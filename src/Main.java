8de46a3ec026ff69e9a36645369ca8efff3f2027
Nandhini M <nandhinimm4706@gmail.com>
2026-03-18 23:45:52 +0530

    main method

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,10 @@ public class Main{
         Student s=new Student("1","Nandhini","20.07.2006","3rd", "IT","99");
         Connection con= DBConnection.getConnection();
 
+        while(true){
+            System.out.println("----------------------------------------------Menu------------------------------------------------");
+            System.out.println("1.Add Student");
+
+        }
     }
 }
