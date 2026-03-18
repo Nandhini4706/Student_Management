a9a101db838530c5df5c8ee8897d2f15d1d4c967
Nandhini M <nandhinimm4706@gmail.com>
2026-03-18 23:50:01 +0530

    main method

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main{
             System.out.println("----------------------------------------------Menu------------------------------------------------");
             System.out.println("1.Add Student");
 System.out.println("2.View Students");
+System.out.println("3.Exir");
         }
+        return null;
     }
 }
