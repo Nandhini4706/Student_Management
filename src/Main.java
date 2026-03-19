a820e70625f0d95823e5385a639d5ebcf5fe476f
Nandhini M <nandhinimm4706@gmail.com>
2026-03-19 23:56:26 +0530

    db connection

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import StudDAO.DAO_Stud;
 import Model.Student;
 public class Main{
     public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
         DAO_Stud dao=new DAO_Stud();
         Student s=new Student("1","Nandhini","20.07.2006","3rd", "IT","99");
         Connection con= DBConnection.getConnection();
@@ -15,6 +16,11 @@ public class Main{
             System.out.println("1.Add Student");
 System.out.println("2.View Students");
 System.out.println("3.Exit");
+int num=sc.nextInt();
+switch(num){
+    case 1:
+
+}
         }
 
     }
