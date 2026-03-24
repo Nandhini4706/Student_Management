96cfbefe50053c02a76585fb1d38015af82db39e
Nandhini M <nandhinimm4706@gmail.com>
2026-03-24 22:23:37 +0530

    switch cases

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main{
         DAO_Stud dao=new DAO_Stud();
         Student s=new Student("1","Nandhini","20.07.2006","3rd", "IT","99");
         Connection con= DBConnection.getConnection();
-
+        System.out.println(con);
         while(true){
             System.out.println("----------------------------------------------Menu------------------------------------------------");
             System.out.println("1.Add Student");
@@ -21,7 +21,12 @@ switch(num){
     case 1:
          System.out.println("Enter student name: ");
          String sub=sc.nextLine();
-         System.out.print("Enter Name: ");
+         System.out.print("Enter Dept Name: ");
+         String dept=sc.nextLine();
+         System.out.println("Enter Phone Number: ");
+         String phone=sc.nextLine();
+         System.out.println("Enter Age");
+         String age=sc.nextLine();
 }
         }
 
