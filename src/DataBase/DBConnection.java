36f30e63eb3203cf8894d1ff7968221db0b7f15e
Nandhini M <nandhinimm4706@gmail.com>
2026-03-24 22:48:37 +0530

    switch cases

--- src/DataBase/DBConnection.java
+++ src/DataBase/DBConnection.java
@@ -2,19 +2,18 @@ package DataBase;
 import DataBase.DBConnection;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.concurrent.Callable;
 
 public class DBConnection {
 public static Connection getConnection() {
           Connection con=null;
           try{
-              String url="jdbc:mysql://localhost:3306/student_db";
+                String url="jdbc:mysql://localhost:3306/student_db";
                 String username="root";
                 String password="Nandhu07";
                 con = DriverManager.getConnection(url,username,password);
                 System.out.println("Database");
           }catch(Exception e){
-              e.printStackTrace();
+              e.printStackTrace(); //to print detailed about error in lines
           }
     return con;
     }
--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main{
         DAO_Stud dao=new DAO_Stud();
         Student s=new Student("1","Nandhini","20.07.2006","3rd", "IT","99");
         Connection con= DBConnection.getConnection();
-        System.out.println(con);
+       // System.out.println(con);
         while(true){
             System.out.println("----------------------------------------------Menu------------------------------------------------");
             System.out.println("1.Add Student");
@@ -27,6 +27,7 @@ switch(num){
          String phone=sc.nextLine();
          System.out.println("Enter Age");
          String age=sc.nextLine();
+
 }
         }
 
