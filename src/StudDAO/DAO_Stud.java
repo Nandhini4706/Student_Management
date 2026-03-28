10b48178d1a5585b1fc6ab0c601e02671e4587ae
Nandhini M <nandhinimm4706@gmail.com>
2026-03-28 23:59:42 +0530

    entity connection

--- src/StudDAO/DAO_Stud.java
+++ src/StudDAO/DAO_Stud.java
@@ -1,17 +1,24 @@
 package StudDAO;
 import DataBase.DBConnection;
 import Model.Student;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
+import java.io.InputStream;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class DAO_Stud {
        ArrayList<Student> student;
        public void addStudent(Student students){
            student.add(students);
        }
-        public void viewStudent(){
+        public void viewStudent()throws Exception{
+          String query="SELECT* FROM student";
+          PreparedStatement ps1=con.PreparedStatement(){
+          int yt
 
         }
 }
